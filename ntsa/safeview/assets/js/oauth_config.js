4ac75b04b980a310cafc5ef7c14ab9aa269b7820
NexxaBini <freelancerbini@gmail.com>
2024-11-29 21:56:25 +0900

    Update oauth_config.js

--- ntsa/safeview/assets/js/oauth_config.js
+++ ntsa/safeview/assets/js/oauth_config.js
@@ -2,5 +2,6 @@ const OAUTH_CONFIG = {
     CLIENT_ID: 'YOUR_CLIENT_ID',
     API_KEY: 'AIzaSyDyGZ7lrR_SkdSYMdC7EdMTujQ4Yav_bHk',
     SCOPES: 'https://www.googleapis.com/auth/spreadsheets.readonly',
-    DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+    DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    REDIRECT_URI: 'https://nexxa.kro.kr/ntsa/safeview/oauth-callback'
 };
